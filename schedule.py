8642e5520a07cbe79e0994d2ea41ef71ce1f8f07
Dmitriy Stepanenko <101544727+d-step-co@users.noreply.github.com>
2022-04-28 16:14:43 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user=True)
-job = cron.new(command='python /Users/mymacbook/Documents/Work/PyCharm/demo_ikea/get_data.py')
+job = cron.new(command='python /user_path/demo_ikea/get_data.py')
 job.minute.every(15)
 
 cron.write()
